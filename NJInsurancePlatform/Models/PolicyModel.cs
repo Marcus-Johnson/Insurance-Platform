fa3893700477397e91ad68940484907e784319a5
Alexander Soler <asoler@Galaxe.com>
2022-10-26 16:34:36 -0400

    Added Policy Model

---
+++ NJInsurancePlatform/Models/PolicyModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NJInsurancePlatform.Models
+{
+    public class PolicyModel
+    {
+        [Key]
+
+        public Guid PolicyMUID { get; set; }
+
+        public int PolicyNumber { get; set; }
+
+        public string NameOfPolicy { get; set; }
+
+        public string PolicyOwner { get; set; }
+
+        public Boolean PolicyPaymentisDue { get; set; }
+
+        public double PolicyTotalAmount { get; set; }
+
+        public double PolicyPaidOffAmount { get; set; }
+
+        public DateTime PolicyStart_Date { get; set; }
+
+        public DateTime PolicyEnd_Date { get; set; }
+
+    }
+}
