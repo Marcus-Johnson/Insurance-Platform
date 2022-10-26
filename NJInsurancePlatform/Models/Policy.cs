f8ca919d49bc4907c934137b7db6b895194ecfeb
Alexander Soler <asoler@Galaxe.com>
2022-10-26 17:32:02 -0400

    changes1

--- NJInsurancePlatform/Models/Policy.cs
+++ NJInsurancePlatform/Models/Policy.cs
@@ -2,26 +2,32 @@
 
 namespace NJInsurancePlatform.Models
 {
-    public class PolicyModel
+    public partial class Policy
     {
         [Key]
-
         public Guid PolicyMUID { get; set; }
 
+        [Required(ErrorMessage = "Missing - PolicyNumber")]
         public int PolicyNumber { get; set; }
 
+        [Required(ErrorMessage = "Missing - Name Of Policy")]
+        [StringLength(50)]
         public string NameOfPolicy { get; set; }
 
+        [Required(ErrorMessage = "Missing - Policy Owner")]
+        [StringLength(50)]
         public string PolicyOwner { get; set; }
-
+        
         public Boolean PolicyPaymentisDue { get; set; }
 
+        [Required(ErrorMessage = "Missing - Minimum Payment")]
         public double PolicyTotalAmount { get; set; }
 
+        [Required(ErrorMessage = "Missing - Minimum Payment")]
         public double PolicyPaidOffAmount { get; set; }
-
+        [Required(ErrorMessage = "Missing - Policy Due Date")]
         public DateTime PolicyStart_Date { get; set; }
-
+        [Required(ErrorMessage = "Missing - Policy End Date")]
         public DateTime PolicyEnd_Date { get; set; }
 
     }
