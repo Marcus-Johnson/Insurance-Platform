5dbf7b3161224a88372eba68a6b2495567a41235
Zaleski <zzaleski@galaxe.com>
2022-10-26 16:45:00 -0400

    CENJ-8 Pushing up model in NJInsurancePlatform/Models/

--- NJInsurancePlatform/Models/Bill.cs
+++ NJInsurancePlatform/Models/Bill.cs
@@ -1,6 +1,30 @@
-﻿namespace NJInsurancePlatform.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NJInsurancePlatform.Models
 {
-    public class Bill
+    public partial class Bill
     {
+        [Key]
+        public int BillMUID { get; set; }
+
+        [ForeignKey("PolicyMUID")]
+        public virtual int PolicyMUID { get; set; }
+
+        [Required]
+        public DateTime PolicyDueDate { get; set; }
+
+        [Required]
+        public double MinimumPayment { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public double Balance { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string? Status { get; set; }
     }
 }
