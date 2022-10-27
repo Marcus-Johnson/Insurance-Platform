e56a269d0a79afe29b0457adb1c0db6f6a0f2f61
Alexander Soler <asoler@Galaxe.com>
2022-10-27 10:21:17 -0400

    added display name and sacffold false

--- NJInsurancePlatform/Models/Policy.cs
+++ NJInsurancePlatform/Models/Policy.cs
@@ -2,33 +2,42 @@
 
 namespace NJInsurancePlatform.Models
 {
-    public partial class Policy
+    public class Policy
     {
         [Key]
+        [ScaffoldColumn(false)]
         public Guid PolicyMUID { get; set; }
 
+        [DisplayName("PolicyNumber")]
         [Required(ErrorMessage = "Missing - PolicyNumber")]
         public int PolicyNumber { get; set; }
 
+        [DisplayName("NameOfPolicy")]
         [Required(ErrorMessage = "Missing - Name Of Policy")]
         [StringLength(50)]
         public string NameOfPolicy { get; set; }
 
+        [DisplayName("PolicyOwner")]
         [Required(ErrorMessage = "Missing - Policy Owner")]
         [StringLength(50)]
         public string PolicyOwner { get; set; }
-        
+
+        [DisplayName("PolicyPaymentisDue")]
         public Boolean PolicyPaymentisDue { get; set; }
 
+        [DisplayName("PolicyTotalAmount")]
         [Required(ErrorMessage = "Missing - Minimum Payment")]
         public double PolicyTotalAmount { get; set; }
 
+        [DisplayName("PolicyPaidOffAmount")]
         [Required(ErrorMessage = "Missing - Minimum Payment")]
         public double PolicyPaidOffAmount { get; set; }
 
+        [DisplayName("PolicyStart_Date")]
         [Required(ErrorMessage = "Missing - Policy Due Date")]
         public DateTime PolicyStart_Date { get; set; }
 
+        [DisplayName("PolicyEnd_Date")]
         [Required(ErrorMessage = "Missing - Policy End Date")]
         public DateTime PolicyEnd_Date { get; set; }
 
