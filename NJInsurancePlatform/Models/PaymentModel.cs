2b87c2a950811d03c175bb50ecc248f958615ca2
JDevXV <jdevxv@gmail.com>
2022-10-27 10:57:02 -0400

    CENJ-10 Added Complete Payment Model

--- NJInsurancePlatform/Models/PaymentModel.cs
+++ NJInsurancePlatform/Models/PaymentModel.cs
@@ -1,6 +1,88 @@
-﻿namespace NJInsurancePlatform.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace NJInsurancePlatform.Models
 {
     public class PaymentModel
     {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int PaymentMUID { get; set; }
+
+        [ForeignKey("BillMUID")]
+        [ScaffoldColumn(false)]
+        public virtual int BillMUID { get; set; }
+
+        [Required(ErrorMessage = "Paid Date is required.")]
+        [DisplayName("Paid Date")]
+        public DateTime PaidDate { get; set; }
+
+        [Required(ErrorMessage = "Amount is required.")]
+        [DisplayName("Amount")]
+        public double Amount { get; set; }
+
+        [Required(ErrorMessage = "Payment Method is required.")]
+        [DisplayName("Payment Method")]
+        public string? PaymentMethod { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        [DisplayName("Payer First Name")]
+        public string? PayerFirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [DisplayName("Payer Last Name")]
+        public string? PayerLastName { get; set; }
+
+        [Required(ErrorMessage = "Missing - Card Number")]
+        [RegularExpression("^[0-9]*$")]
+        [StringLength(16)]
+        [DisplayName("Card Number")]
+        public string? CardNumber { get; set; }
+
+        [Required(ErrorMessage = "Missing - Zip Code")]
+        [RegularExpression("^[0-9]*$")]
+        [StringLength(5)]
+        [DisplayName("Zip Code")]
+        public string? ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Missing - Card Expire Date")]
+        [DisplayName("Card Expire Date")]
+        public DateTime CardExpireDate { get; set; }
+
+        [Required(ErrorMessage = "Missing - Debit or Credit")]
+        [DisplayName("Debit or Credit")]
+        public string? DebitOrCredit { get; set; }
+
+        [Required(ErrorMessage = "Missing - Bank Name")]
+        [DisplayName("Bank Name")]
+        public string? BankName { get; set; }
+
+        [Required(ErrorMessage = "Missing - Account Number must be numeric.")]
+        [RegularExpression("^[0-9]*$")]
+        [DisplayName("Account Number")]
+        public string? AccountNumber { get; set; }
+
+        [Required(ErrorMessage = "Missing - Routing Number must be numeric.")]
+        [StringLength(9)]
+        [RegularExpression("^[0-9]*$")]
+        [DisplayName("Routing Number")]
+        public string? RoutingNumber { get; set; }
+
+        [Required(ErrorMessage = "Missing - Check Number")]
+        [DisplayName("Check Number")]
+        public int CheckNumber { get; set; }
+
+        [Required(ErrorMessage = "Missing - Check Image")]
+        [DisplayName("Account Number")]
+        public IFormFile? CheckImage { get; set; }
+
+        [Required(ErrorMessage = "Additional Info is required.")]
+        [DisplayName("Additional Info")]
+        public string? AdditionalInfo { get; set; }
+
+        [Required(ErrorMessage = "Created Date is required.")]
+        [DisplayName("Created Date")]
+        public DateTime CreatedDate { get; set; }
     }
 }
