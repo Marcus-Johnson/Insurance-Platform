65e50eb1720d4bfb1c379d38dc1731ba6a3f5cc4
Rodriguez-Guillen <arodriguezguillen@galaxe.com>
2022-10-27 11:43:01 -0400

    this is the 1st update to the customer model

--- NJInsurancePlatform/Models/CustomerModel.cs
+++ NJInsurancePlatform/Models/CustomerModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NJInsurancePlatform.Models
@@ -6,35 +7,48 @@ namespace NJInsurancePlatform.Models
     public class CustomerModel
     {
         [Key]
-        public int CustomerMuid { get; set; }
-        [ForeignKey("PolicyModel")] // Change the argument here so that it matches the name of the policy model
         [ScaffoldColumn(false)]
-        public int PolicyMuid { get; set; }
+        [DisplayName("Customer ID")]
+        public Guid CustomerMUID { get; set; }
+        [ForeignKey("Policy")]
+        [ScaffoldColumn(false)]
+        [DisplayName("Policy ID")]
+        public Guid PolicyMUID { get; set; }
         [Required(ErrorMessage = "Missing field: First Name")]
         [StringLength(140)]
-        public string FirstName { get; set; }
+        [DisplayName("First Name")]
+        public string? FirstName { get; set; }
         [Required(ErrorMessage = "Missing field: Last Name")]
         [StringLength(140)]
-        public string LastName { get; set; }
+        [DisplayName("Last Name")]
+        public string? LastName { get; set; }
         [Required(ErrorMessage = "Missing field: Email Address")]
         [StringLength(200)]
-        public string EmailAddress { get; set; }
+        [DisplayName("Email Address")]
+        public string? EmailAddress { get; set; }
         [Required(ErrorMessage = "Missing field: Phone Number")]
         [StringLength(10)]
-        public string PhoneNumber { get; set; }
+        [DisplayName("Phone Number")]
+        public string? PhoneNumber { get; set; }
         [Required(ErrorMessage = "Missing field: SSN")]
         [StringLength(9)]
-        public string SSN { get; set; }
+        [DisplayName("SSN")]
+        public string? SSN { get; set; }
         [Required(ErrorMessage = "Missing field: License Number")]
         [StringLength(15)]
-        public string LicenseNumber { get; set; }
-        [Required(ErrorMessage = "Missing field: Primary policy holder?")]
+        [DisplayName("License Number")]
+        public string? LicenseNumber { get; set; }
+        [Required(ErrorMessage = "Missing field: Are you the primary policy holder?")]
+        [DisplayName("Are you the primary policy holder?")]
         public bool IsPrimaryPolicyHolder { get; set; }
         [Required(ErrorMessage = "Missing field: Gender")]
+        [DisplayName("Gender")]
         public bool Gender { get; set; }
         [Required(ErrorMessage = "Missing field: Date Created")]
+        [DisplayName("Date Created")]
         public string CreatedDate { get; set; } = null!;
         [Required(ErrorMessage = "Missing field: Is this policy active?")]
+        [DisplayName("Is this policy active?")]
         public bool Active { get; set; }
 
     }
