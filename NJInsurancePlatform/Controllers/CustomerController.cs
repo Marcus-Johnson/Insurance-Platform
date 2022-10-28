0446a07dd671ea5d74943389e2e096cb274a9cdc
lancecordel <lancecordel@gmail.com>
2022-10-28 12:02:46 -0400

    Customer Controller

---
+++ NJInsurancePlatform/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NJInsurancePlatform.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
