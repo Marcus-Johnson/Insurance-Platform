e16f822c311ce6467ffc10976d9a0bb9e16d12fb
Zaleski <zzaleski@galaxe.com>
2022-10-28 11:28:41 -0400

    Created the BillController.cs file and added imports we will most likely need later

---
+++ NJInsurancePlatform/Controllers/BillController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NJInsurancePlatform.Models;
+using NJInsurancePlatform.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+//using NJInsurancePlatform.Repositories;
+
+
+namespace NJInsurancePlatform.Controllers
+{
+    public class BillController
+    {
+    }
+}
+
