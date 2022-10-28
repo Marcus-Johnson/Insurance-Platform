1e0269d79ab234b49dbdd79aa040d3603410a1b3
Alexander Soler <asoler@Galaxe.com>
2022-10-28 13:40:18 -0400

    Added Policy Controller with some extra

---
+++ NJInsurancePlatform/Controllers/PolicyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NJInsurancePlatform.Models;
+using NJInsurancePlatform.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace NJInsurancePlatform.Controllers
+{
+	public class PolicyController : Controller
+	{
+		private readonly InsuranceCorpDbContext InsuranceCorpDbContext;
+
+		public PolicyController(InsuranceCorpDbContext InsuranceCorpDbContext)
+		{
+			this.InsuranceCorpDbContext = InsuranceCorpDbContext;
+		}
+	}
+}
