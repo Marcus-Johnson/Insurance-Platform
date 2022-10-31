7f93a9631134e028181d583a4b26f459cde7aa1a
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2022-10-31 15:59:59 -0400

    added paymentDetails view page, and added it to nav (#18)
    * added paymentDetails view page, and added it to nav
    
    * Update Details.cshtml
    
    removed an auxiliary link
    
    * Update Details.cshtml
    
    added requirements for the payment form
    
    * Update Details.cshtml
    
    readded the lines you requested
    
    Co-authored-by: Zaleski <zzaleski@galaxe.com>

--- NJInsurancePlatform/Controllers/PaymentController.cs
+++ NJInsurancePlatform/Controllers/PaymentController.cs
@@ -4,7 +4,20 @@ namespace NJInsurancePlatform.Controllers
 {
     public class PaymentController : Controller
     {
+
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
         {
             return View();
         }
