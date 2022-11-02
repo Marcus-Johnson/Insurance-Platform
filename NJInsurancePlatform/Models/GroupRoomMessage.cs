c9e3968c00dfa0389836b9fbf357542678428d98
Zaleski <zzaleski@galaxe.com>
2022-11-02 16:43:47 -0400

    Created the GroupRoomMessage model

---
+++ NJInsurancePlatform/Models/GroupRoomMessage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NJInsurancePlatform.Models
+{
+    public class GroupRoomMessage
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        [DisplayName("Group Room Message MUID")]
+        public Guid GroupRoomMessageMUID { get; set; }
+
+
+        [ForeignKey("GroupRoom")]
+        [ScaffoldColumn(false)]
+        [DisplayName("GroupMUID")]
+        public Guid GroupRoomMUID { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Required(ErrorMessage = "Missing field: Sender MUID")]
+        public Guid SenderMUID { get; set; }
+
+        [Required(ErrorMessage = "Missing field: Message")]
+        [StringLength(140)]
+        [DisplayName("Message")] 
+        public string? Message { get; set; }
+    }
+}
