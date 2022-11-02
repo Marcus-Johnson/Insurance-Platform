c59e26b5a68bf2c429e7f601b4e8c9076ddbbdb8
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2022-11-02 15:45:15 -0400

    Created GroupRoom model (#39)
    Co-authored-by: Zaleski <zzaleski@galaxe.com>

---
+++ NJInsurancePlatform/Models/GroupRoom.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NJInsurancePlatform.Models
+{
+    public class GroupRoom
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        [DisplayName("Group MUID")]
+        public Guid GroupMUID { get; set; }
+
+        [Required(ErrorMessage = "Missing field: Name")]
+        [StringLength(140)]
+        [DisplayName("Name")]
+        public string? Name { get; set; }
+    }
+}
