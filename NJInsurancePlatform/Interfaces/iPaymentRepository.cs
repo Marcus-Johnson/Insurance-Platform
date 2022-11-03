6699c2c6b94182b4e7f871d0e92d9f84b27d8fbf
Jailene DeVine-Jones <jdevxv@gmail.com>
2022-11-03 12:30:34 -0400

    CENJ 58 addition add i payment repository interface to codebase (#54)
    * Added IPaymentRepository to Codebase
    
    * Added IPaymentRepository to Codebase
    
    * Added inheritance for IDisposable as requested

---
+++ NJInsurancePlatform/Interfaces/iPaymentRepository.cs
@@ -0,0 +1,19 @@
+﻿using NJInsurancePlatform.Models;
+
+namespace NJInsurancePlatform.Interfaces
+{
+    public interface IPaymentRepository : IDisposable
+    {
+        IEnumerable<Payment> GetPayment();
+
+        Payment GetPaymentsByID(Guid PaymentMUID);
+
+        void InsertPayment(Payment payment);
+
+        void DeletePayment(Guid PaymentMUID);
+
+        void UpdatePayment(Payment payment);
+
+        void Save();
+    }
+}
