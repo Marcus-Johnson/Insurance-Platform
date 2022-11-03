4d604182a140d97e725a8e7a8ad3e5a674c97352
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2022-11-03 13:56:32 -0500

    Readability fix

--- NJInsurancePlatform/Interfaces/PolicyRepository.cs
+++ NJInsurancePlatform/Interfaces/PolicyRepository.cs
@@ -7,24 +7,29 @@ namespace NJInsurancePlatform.Interfaces
     public class PolicyRepository : iPolicyRepository
     {
         private readonly InsuranceCorpDbContext _databaseContext;
+        
         public async Task<IEnumerable<Policy>> GetPolicies()
         {
             return _databaseContext.Policies.ToList();
         }
+        
         public async Task<Customer> GetPoliciesByID(Guid PolicyMUID)
         {
             var customer = await _databaseContext.Customers.FindAsync(PolicyMUID);
             return customer;
         }
+        
         public async void InsertPolicy(Policy policy)
         {
             await _databaseContext.Policies.AddAsync(policy);
         }
+        
         public async void DeletePolicy(Guid PolicyMUID)
         {
             var policyRemove = _databaseContext.Policies.FirstOrDefault(p => p.PolicyMUID == PolicyMUID);
             _databaseContext.Policies.Remove(policyRemove);
         }
+        
         public async void UpdatePolicy(Policy policy)
         {
             try
@@ -36,6 +41,7 @@ namespace NJInsurancePlatform.Interfaces
                 throw;
             }
         }
+        
         public async void Save()
         {
             await _databaseContext.SaveChangesAsync();
