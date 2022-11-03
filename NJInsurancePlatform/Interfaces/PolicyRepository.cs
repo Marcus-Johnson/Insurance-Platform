10b260ed87e814903eae21cd19f9e20846597d78
Alexander Soler <asoler@Galaxe.com>
2022-11-03 14:53:36 -0400

    fixed spacing

--- NJInsurancePlatform/Interfaces/PolicyRepository.cs
+++ NJInsurancePlatform/Interfaces/PolicyRepository.cs
@@ -7,30 +7,24 @@ namespace NJInsurancePlatform.Interfaces
     public class PolicyRepository : iPolicyRepository
     {
         private readonly InsuranceCorpDbContext _databaseContext;
-
-
         public async Task<IEnumerable<Policy>> GetPolicies()
         {
             return _databaseContext.Policies.ToList();
         }
-
         public async Task<Customer> GetPoliciesByID(Guid PolicyMUID)
         {
             var customer = await _databaseContext.Customers.FindAsync(PolicyMUID);
             return customer;
         }
-
         public async void InsertPolicy(Policy policy)
         {
             await _databaseContext.Policies.AddAsync(policy);
         }
-
         public async void DeletePolicy(Guid PolicyMUID)
         {
             var policyRemove = _databaseContext.Policies.FirstOrDefault(p => p.PolicyMUID == PolicyMUID);
             _databaseContext.Policies.Remove(policyRemove);
         }
-
         public async void UpdatePolicy(Policy policy)
         {
             try
@@ -42,14 +36,9 @@ namespace NJInsurancePlatform.Interfaces
                 throw;
             }
         }
-
         public async void Save()
         {
             await _databaseContext.SaveChangesAsync();
         }
-
-       
-
-      
     }
 }
