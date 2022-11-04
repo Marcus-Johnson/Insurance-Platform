4a3c1b6f909bfaff2865199507cd8c6e67eacc1e
lancecordel <64986141+lancecordel@users.noreply.github.com>
2022-11-04 17:42:52 -0400

    Created Claim Model (#71)

---
+++ NJInsurancePlatform/Models/Claim.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NJInsurancePlatform.Models
+{
+    public class Claim
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        [DisplayName("Claim Id")]
+        public Guid ClaimMUID { get; set; }
+
+        [ForeignKey("CustomerMUID")]
+        [ScaffoldColumn(false)]
+        public string? CustomerMUID { get; set; }
+
+        [ForeignKey("PolicyMUID")]
+        [ScaffoldColumn(false)]
+        public string? PolicyMUID { get; set; }
+
+        [Required(ErrorMessage = "Missing Field: Claim Description")]
+        [StringLength(500)]
+        [DisplayName("Claim Description")]
+        public string? ClaimUserDescription { get; set; }
+
+        [Required(ErrorMessage = "Missing field: Date Of Claim")]
+        [DisplayName("Date Of Claim")]
+        public DateTime DateOfClaim { get; set; }
+    }
+
+}
