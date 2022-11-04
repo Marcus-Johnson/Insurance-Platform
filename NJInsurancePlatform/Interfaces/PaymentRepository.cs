075f7c9eb905c4ce59c78a9e622dc89ffb051d5f
JDevXV <jdevxv@gmail.com>
2022-11-04 10:25:40 -0400

    Added PaymentRepository functions to codebase

---
+++ NJInsurancePlatform/Interfaces/PaymentRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NJInsurancePlatform.Data;
+using NJInsurancePlatform.Models;
+
+namespace NJInsurancePlatform.Interfaces
+{
+    public class PaymentRepository : iPaymentRepository
+    {
+        private readonly InsuranceCorpDbContext _databaseContext;
+        public PaymentRepository(InsuranceCorpDbContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public async Task<IEnumerable<Payment>> GetPayment()
+        {
+            return await _databaseContext.Payments.ToListAsync();
+        }
+
+        public async Task<Payment> GetPaymentsByID(Guid PaymentMUID)
+        {
+            var payment = await _databaseContext.Payments.FindAsync(PaymentMUID);
+            return payment;
+        }
+
+        public async void InsertPayment(Payment payment)
+        {
+            await _databaseContext.Payments.AddAsync(payment);
+        }
+
+        public async void DeletePayment(Guid PaymentMUID)
+        {
+            var removePayment = await _databaseContext.Payments.FirstOrDefaultAsync(p => p.PaymentMUID == PaymentMUID);
+            _databaseContext.Payments.Remove(removePayment);
+        }
+
+        public async void UpdatePayment(Payment payment)
+        {
+            try
+            {
+                _databaseContext.Update(payment);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+        }
+
+        public async void Save()
+        {
+            await _databaseContext.SaveChangesAsync();
+        }
+    }
+}
