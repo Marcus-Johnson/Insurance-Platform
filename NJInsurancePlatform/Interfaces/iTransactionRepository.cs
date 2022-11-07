7cde27246e1629cc47c620be57cd3b2fc1e28578
Jailene DeVine-Jones <jdevxv@gmail.com>
2022-11-07 12:19:20 -0500

    Added iTransactionRepository (#75)

---
+++ NJInsurancePlatform/Interfaces/iTransactionRepository.cs
@@ -0,0 +1,20 @@
+﻿using NJInsurancePlatform.Models;
+using System;
+
+namespace NJInsurancePlatform.Interfaces
+{
+    public interface ITransactionRepository : IDisposable
+    {
+        Task<IEnumerable<Transaction>> GetTransactions();
+
+        Task<Transaction> GetTransactionsByID(Guid TransactionMUID);
+
+        void InsertTransaction(Transaction transaction);
+
+        void DeleteTransaction(Guid TransactionMUID);
+
+        void UpdateTransaction(Transaction transaction);
+
+        void Save();
+    }
+}
