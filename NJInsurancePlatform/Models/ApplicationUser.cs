dcdb491ad7368d244f998bbdf784c94566d3044b
lancecordel <lancecordel@gmail.com>
2022-11-07 14:48:15 -0500

    finished Layout.  Testing functionality of buttons

--- NJInsurancePlatform/Models/ApplicationUser.cs
+++ NJInsurancePlatform/Models/ApplicationUser.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace NJInsurancePlatform.Models
 {
     public class ApplicationUser : IdentityUser
     {
         // Extended Attributes HERE
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
         public string ExtendedAttribueExample { get; set; }
     }
 }
