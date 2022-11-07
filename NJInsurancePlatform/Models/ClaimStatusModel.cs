2d22995e521ce6e9c31da323ae4ca47b5bddca7d
JDevXV <jdevxv@gmail.com>
2022-11-07 11:34:23 -0500

    Created Claim Status Model.

---
+++ NJInsurancePlatform/Models/ClaimStatusModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace NJInsurancePlatform.Models
+{
+    public class ClaimStatusModel
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        [DisplayName("ClaimMUID")]
+        public Guid ClaimMUID { get; set; }
+
+        [Required(ErrorMessage = "Missing field: Is Pending")]
+        [DisplayName("Is Pending")]
+        public bool isPending { get; set; } = true;
+
+        [Required(ErrorMessage = "Missing field: Is Complete")]
+        [DisplayName("Is Complete")]
+        public bool isComplete { get; set; }
+    }
+}
