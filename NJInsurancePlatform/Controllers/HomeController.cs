7e5325dd4e9cf91efe8c0bbfed3063baf38fb4a0
Jailene DeVine-Jones <jdevxv@gmail.com>
2022-11-08 20:02:32 -0500

    CENJ-93-addition-message-view-containing-wireframe (#93)
    * CENJ-93-addition-message-view-containing-wireframe
    
    * Removed colors as requested.
    
    * Added speech bubbles and made changes as requested.
    
    * Added Icon to submit button as requested.

--- NJInsurancePlatform/Controllers/HomeController.cs
+++ NJInsurancePlatform/Controllers/HomeController.cs
@@ -136,5 +136,10 @@ namespace NJInsurancePlatform.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
     }
 }
