14b64bcaa6f014fa3d38cf1381c453de5a171ef7
Alex S <55304780+SolerJapan@users.noreply.github.com>
2022-11-11 14:50:57 -0500

    CENJ-127 -model-faq (#114)
    CENJ-128-addition-add-faq-model-to-codebase
    
    Co-authored-by: Alexander Soler <asoler@Galaxe.com>

---
+++ NJInsurancePlatform/Models/FAQ.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace NJInsurancePlatform.Models
+{
+    public class Faq
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public Guid FaqMUID { get; set; }
+
+        [DisplayName("Question")]
+        [Required(ErrorMessage = "Missing - Question")]
+        public string? Question { get; set; }
+
+        [DisplayName("Answer")]
+        [Required(ErrorMessage = "Missing - Answer")]
+        public string? Answer { get; set; }
+
+        [DisplayName("Date Created")]
+        [Required(ErrorMessage = "Missing - Date Created")]
+        public TimestampAttribute DateCreated { get; set; }
+    }
+}
