845ff8233fc21843c2a793bd7143b0c7ca13ebe7
lancecordel <lancecordel@gmail.com>
2022-11-14 11:34:33 -0500

    add "Paid" actions and views

--- NJInsurancePlatform/Controllers/Administration.cs
+++ NJInsurancePlatform/Controllers/Administration.cs
@@ -71,24 +71,79 @@ namespace NJInsurancePlatform.Controllers
         }
 
 
+        // GET PAID TRANSACTIONS "GET REQUEST" ------------------------------------------------------------------------------------------------------
+        [HttpGet]
+        public async Task<IActionResult> PaidTransactions()
+        {
+            var transactions = await _transactionRepository.GetTransactions();               // get Transactions from Database
+            var model = new AccountManager();
+
+            foreach (var transaction in transactions)                                        // populate AccountManager Transactions List
+            {
+                if (transaction.isPaymentComplete == true)
+                {
+                    model.Transactions?.Add(transaction);
+                }
+            }
+
+            if (model.Transactions?.Count == 0 || model.Transactions == null) return RedirectToAction("NoPaymentsComplete", "Administration");
+
+            return View(model);                                                             // Pass AccountManager to View
+        }
+
+
+        // GET PAID TRANSACTIONS "GET POST" ------------------------------------------------------------------------------------------------------
+        [HttpPost]
+        public async Task<IActionResult> PaidTransactions(AccountManager model)
+        {
+
+            for (int i = 0; i < model.Transactions?.Count; i++)
+            {
+                if (model.Transactions[i].isPaymentComplete == false)
+                {
+                    _transactionRepository.UpdateTransaction(model.Transactions[i]);            // Update transactions
+                }
+
+                if (i < (model.Transactions.Count - 1)) continue;                           // If iteration is not complete
+
+                else
+                {
+                    _transactionRepository.Save();
+                }
+            }
+
+            return RedirectToAction("UpdateRecorded", "Administration");
+        }
+
+
+
         // ACCESS DENIED "GET REQUEST" ---------------------------------------------------------------------------------------------------------
         [HttpGet]
         [AllowAnonymous]
         public IActionResult AccessDenied()
         {
             return View();
-        }        
-        
-        
+        }
+
+
         // ALL PAYMENTS COMPLETE "GET REQUEST" ---------------------------------------------------------------------------------------------------------
         [HttpGet]
         [AllowAnonymous]
         public IActionResult AllPaymentsComplete()
         {
             return View();
-        }        
-        
-        
+        }
+
+
+        // ALL PAYMENTS COMPLETE "GET REQUEST" ---------------------------------------------------------------------------------------------------------
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult NoPaymentsComplete()
+        {
+            return View();
+        }
+
+
         // COMPLETE PAYMENT RECORDED "GET REQUEST" ---------------------------------------------------------------------------------------------------------
         [HttpGet]
         [AllowAnonymous]
