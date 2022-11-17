344d0fba7bf738f1299667e7f7bb05fec361ac6f
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2022-11-17 14:13:46 -0600

    Removed comments, Protected method && Void action

--- NJInsurancePlatform/Interfaces/CustomerRepository.cs
+++ NJInsurancePlatform/Interfaces/CustomerRepository.cs
@@ -9,35 +9,37 @@ namespace NJInsurancePlatform.Interfaces
     public class CustomerRepository : ICustomerRepository, IDisposable
     {
         private readonly InsuranceCorpDbContext _dbContext;
+        private bool disposed = false;
+        
         public CustomerRepository(InsuranceCorpDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        // GET ALL ACTION
+        
         public async Task<IEnumerable<Customer>> GetCustomer()
         {
             return await _dbContext.Customers.ToListAsync();
         }
 
-        // GET BY ID ACTION
+        
         public async Task<Customer> GetCustomerById(Guid CustomerMUID)
         {
             var customer = await _dbContext.Customers.FindAsync(CustomerMUID);
             return customer;
         }
-        // ADD ACTION
+        
         public async void InsertCustomer(Customer customer)
         {
             await _dbContext.Customers.AddAsync(customer);
         }
-        // DELETE ACTION
+        
         public async void DeleteCustomer(Guid CustomerMUID)
         {
             var customer = await _dbContext.Customers.FirstOrDefaultAsync(p => p.CustomerMUID == CustomerMUID);
             _dbContext.Customers.Remove(customer);
         }
-        // UPDATE ACTION
+        
         public async void UpdateCustomer(Customer customer)
         {
             try
@@ -49,15 +51,28 @@ namespace NJInsurancePlatform.Interfaces
                 throw;
             }
         }
-        // SAVE ACTION
+        
         public async void Save()
         {
             await _dbContext.SaveChangesAsync();
         }
-        // DISPOSE NOT IN USE
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
