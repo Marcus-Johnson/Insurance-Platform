2d0c9ae19792b7669d8e99290de907d15633e470
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2022-11-18 17:30:00 -0500

    Created GroupRoomController file in controller folder (#138)
    Co-authored-by: Zaleski <zzaleski@galaxe.com>

---
+++ NJInsurancePlatform/Controllers/GroupRoomController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NJInsurancePlatform.Models;
+using NJInsurancePlatform.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace NJInsurancePlatform.Controllers
+{
+    [AllowAnonymous]
+    public class GroupRoomController : Controller
+    {
+        private readonly InsuranceCorpDbContext InsuranceCorpDbContext;
+
+        public GroupRoomController(InsuranceCorpDbContext InsuranceCorpDbContext)
+        {
+            this.InsuranceCorpDbContext = InsuranceCorpDbContext;
+        }
+    }
+}
+
