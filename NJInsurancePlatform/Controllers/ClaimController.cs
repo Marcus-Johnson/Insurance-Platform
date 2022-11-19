5f6da3367fe277e80f70b91dff112b4f29a5be3f
lancecordel <lancecordel@gmail.com>
2022-11-19 08:04:31 -0500

    Create ClaimController

---
+++ NJInsurancePlatform/Controllers/ClaimController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Diagnostics;
+using NJInsurancePlatform.Models;
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace NJInsurancePlatform.Controllers
+{
+    [AllowAnonymous]
+    public class ClaimController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+ 
+        public ClaimController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
