e2ee0b66ac7592b7488ae5cd56188556f43f3565
lancecordel <lancecordel@gmail.com>
2022-11-18 20:06:43 -0500

    no changes

--- NJInsurancePlatform/Data/InsuranceCorpDbContext.cs
+++ NJInsurancePlatform/Data/InsuranceCorpDbContext.cs
@@ -19,6 +19,7 @@ namespace NJInsurancePlatform.Data
         }
 
         public DbSet<GroupRoomMessage> GroupRoomMessages { get; set; }
+        public DbSet<GroupRoom> GroupRooms { get; set; }
         public DbSet<Policy> Policies { get; set; }
         public DbSet<Payment> Payments { get; set; }
         public DbSet<Bill> Bills { get; set; }
