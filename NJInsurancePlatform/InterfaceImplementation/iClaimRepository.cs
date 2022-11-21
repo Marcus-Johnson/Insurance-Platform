0a3ac34a83509971721d8b8671e4545d61cb60d1
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2022-11-21 11:57:54 -0500

    Cenj 156 addition add iClaimRepository interface to codebase (#145)
    * initial commit
    
    * Created iClaimRepository file and populated it
    
    Co-authored-by: Zaleski <zzaleski@galaxe.com>

---
+++ NJInsurancePlatform/InterfaceImplementation/iClaimRepository.cs
@@ -0,0 +1,20 @@
+﻿using NJInsurancePlatform.Models;
+using System;
+
+namespace NJInsurancePlatform.InterfaceImplementation
+{
+    public interface iClaimRepository : IDisposable
+    {
+        Task<IEnumerable<Claim>> GetClaims();
+
+        Task<Claim> GetClaimsByID(Guid ClaimMUID);
+
+        void InsertClaim(Claim claim);
+
+        void DeleteClaim(Guid GroupMUID);
+
+        void UpdateClaim(Claim claim);
+
+        void Save();
+    }
+}
