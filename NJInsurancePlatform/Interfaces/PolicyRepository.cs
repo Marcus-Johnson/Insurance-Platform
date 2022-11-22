81014dbfb56bdafde2f2886a4bbc6e949959605a
Zaleski <zzaleski@galaxe.com>
2022-11-22 10:43:01 -0500

    retrying this addition.  dont know why the checks failed originally

--- NJInsurancePlatform/Interfaces/PolicyRepository.cs
+++ NJInsurancePlatform/Interfaces/PolicyRepository.cs
@@ -6,45 +6,49 @@ using System;
 
 namespace NJInsurancePlatform.Interfaces
 {
-    public class PolicyRepository : iPolicyRepository, IDisposable
+    public class PolicyRepository : IPolicyRepository, IDisposable
     {
         private readonly InsuranceCorpDbContext _databaseContext;
         private bool disposed = false;
-        
-        public async Task<IEnumerable<Policy>> GetPolicies()
+        public PolicyRepository(InsuranceCorpDbContext databaseContext)
+        {
+            this._databaseContext = databaseContext;
+        }
+
+        public async Task<List<Policy>> GetPolicies()
         {
             return _databaseContext.Policies.ToList();
         }
-        
+
         public async Task<Customer> GetPoliciesByID(Guid PolicyMUID)
         {
             var customer = await _databaseContext.Customers.FindAsync(PolicyMUID);
             return customer;
         }
-        
+
         public async void InsertPolicy(Policy policy)
         {
             await _databaseContext.Policies.AddAsync(policy);
         }
-        
+
         public async void DeletePolicy(Guid PolicyMUID)
         {
             var policyRemove = _databaseContext.Policies.FirstOrDefault(p => p.PolicyMUID == PolicyMUID);
             _databaseContext.Policies.Remove(policyRemove);
         }
-        
+
         public async void UpdatePolicy(Policy policy)
         {
             try
             {
-                 _databaseContext.Update(policy);
+                _databaseContext.Update(policy);
             }
             catch (DbUpdateConcurrencyException)
             {
                 throw;
             }
         }
-        
+
         public async void Save()
         {
             await _databaseContext.SaveChangesAsync();
--- NJInsurancePlatform/Program.cs
+++ NJInsurancePlatform/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Transaction Repository Dependency Injection
 builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<IPolicyRepository, PolicyRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
