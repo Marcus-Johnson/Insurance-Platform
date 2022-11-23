da7890ea28c487e5276b97c9d3cc99fb72c2a185
Adrian <arodrigue7@gmail.com>
2022-11-23 15:35:15 -0500

    CENJ-154: (#171)
    - init commit
    - not fully functional, but app is still operational

---
+++ NJInsurancePlatform/Models/MessagesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NJInsurancePlatform.Models
+{
+    public class MessagesViewModel
+    {
+        public IEnumerable<GroupRoom> groupRooms { get; set; }
+        public IEnumerable<GroupRoomMessage> groupRoomMessages { get; set; }
+     }
+}
