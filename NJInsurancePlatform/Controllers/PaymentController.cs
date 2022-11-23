c7467bdbba02f16c2d39c04e55a92e2742780eed
Zaleski <zzaleski@galaxe.com>
2022-11-23 14:06:34 -0500

    added updateBill method to paymentcontroller

--- NJInsurancePlatform/Controllers/PaymentController.cs
+++ NJInsurancePlatform/Controllers/PaymentController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NJInsurancePlatform.InterfaceImplementation;
 using NJInsurancePlatform.Models;
 
 namespace NJInsurancePlatform.Controllers
@@ -12,9 +14,17 @@ namespace NJInsurancePlatform.Controllers
     {
 
         private readonly ILogger<PaymentController> _logger;
+        private readonly IPolicyRepository PolicyRepository;
+        private readonly ITransactionRepository TransactionRepository;
+        private readonly iBillRepository BillRepository;
+        private readonly IPaymentRepository PaymentRepository;
 
-        public PaymentController(ILogger<PaymentController> logger)
+        public PaymentController(ILogger<PaymentController> logger, IPolicyRepository PolicyRepository, ITransactionRepository TransactionRepository, iBillRepository BillRepository, IPaymentRepository PaymentRepository)
         {
+            this.PolicyRepository = PolicyRepository;
+            this.TransactionRepository = TransactionRepository;
+            this.BillRepository = BillRepository;
+            this.PaymentRepository = PaymentRepository;
             _logger = logger;
         }
 
@@ -39,6 +49,27 @@ namespace NJInsurancePlatform.Controllers
         public IActionResult Details()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBill(Bill bill)
+        {
+
+            Bill updatedBill = new Bill()
+            {
+                BillMUID = bill.BillMUID,
+                PolicyMUID = bill.PolicyMUID,
+                PolicyDueDate = bill.PolicyDueDate,
+                MinimumPayment = bill.MinimumPayment,
+                CreatedDate = bill.CreatedDate,
+                Balance = bill.Balance,
+                Status = bill.Status,
+            };
+
+            BillRepository.UpdateBill(updatedBill);
+            BillRepository.Save();
+            return RedirectToAction("Index");
         }
     }
 }
