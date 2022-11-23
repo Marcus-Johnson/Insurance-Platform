af97c6cecbb35caeaa8f9fad01335f9f49c9c8ae
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2022-11-23 13:19:15 -0500

    added UpdateBill to billController (#169)
    Co-authored-by: Zaleski <zzaleski@galaxe.com>

--- NJInsurancePlatform/Controllers/BillController.cs
+++ NJInsurancePlatform/Controllers/BillController.cs
@@ -4,14 +4,62 @@ using NJInsurancePlatform.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
+using NJInsurancePlatform.InterfaceImplementation;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using NJInsurancePlatform.Interfaces;
+using Microsoft.AspNetCore.Http.Features;
 //using NJInsurancePlatform.Repositories;
 
 
 namespace NJInsurancePlatform.Controllers
 {
     [AllowAnonymous] //WHILE 
-    public class BillController
+    public class BillController : Controller
     {
+        private readonly IPolicyRepository PolicyRepository;
+        private readonly ITransactionRepository TransactionRepository;
+        private readonly iBillRepository BillRepository;
+        private readonly IPaymentRepository PaymentRepository;
+
+        public BillController(IPolicyRepository PolicyRepository, ITransactionRepository TransactionRepository, iBillRepository BillRepository, IPaymentRepository PaymentRepository)
+        {
+            this.PolicyRepository = PolicyRepository;
+            this.TransactionRepository = TransactionRepository;
+            this.BillRepository = BillRepository;
+            this.PaymentRepository = PaymentRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBill(Bill bill)
+        {
+
+            Bill updatedBill = new Bill()
+            {
+                BillMUID = bill.BillMUID,
+                PolicyMUID = bill.PolicyMUID,
+                PolicyDueDate = bill.PolicyDueDate,
+                MinimumPayment = bill.MinimumPayment,
+                CreatedDate = bill.CreatedDate,
+                Balance = bill.Balance,
+                Status = bill.Status,
+            };
+
+            BillRepository.UpdateBill(updatedBill);
+            BillRepository.Save();
+            return RedirectToAction("Index");
+        }
+
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    PolicyRepository.Dispose();
+        //    TransactionRepository.Dispose():
+        //    BillRepository.Dispose();
+        //    PaymentRepository.Dispose();
+        //    base.Dispose(disposing);
+        //}
+
     }
 }
 
