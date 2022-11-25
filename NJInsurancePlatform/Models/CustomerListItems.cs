f19c5476880ba554ed7a635e91ef0624cde6c4dc
lancecordel <64986141+lancecordel@users.noreply.github.com>
2022-11-24 22:05:35 -0500

    Cenj 142 materialize tabs with customer policy details (#173)
    * Customer Account Page
    
    * - Add Customer Policies and Transactions

---
+++ NJInsurancePlatform/Models/CustomerListItems.cs
@@ -0,0 +1,17 @@
+﻿namespace NJInsurancePlatform.Models
+{
+    public class CustomerListItems
+    {
+        public List<Policy>? Policies { get; set; }
+        public List<Transaction>? Transactions { get; set; }
+        public List<Beneficiary>? Beneficiaries { get; set; }
+        public List<string>? PolicyNames  { get; set; }
+        public CustomerListItems()
+        {
+            this.Policies = new List<Policy>();
+            this.Transactions = new List<Transaction>();
+            this.PolicyNames = new List<string>();
+            this.Beneficiaries = new List<Beneficiary>();
+        }
+    }
+}
