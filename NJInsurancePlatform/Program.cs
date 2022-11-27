6f978d2a148dc41c5aa46fbe6859451264ebec7e
lancecordel <lancecordel@gmail.com>
2022-11-26 23:51:36 -0500

    - Add Dependency Injection for IBillRepository To Program.cs

--- NJInsurancePlatform/Program.cs
+++ NJInsurancePlatform/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
 builder.Services.AddScoped<IPolicyRepository, PolicyRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<iBillRepository, BillRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
