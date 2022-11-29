9283a248035394e6c113beb49cdb782082bb3a6a
lancecordel <lancecordel@gmail.com>
2022-11-29 14:15:10 -0500

    add contructor to MessagesViewModel

--- NJInsurancePlatform/Models/MessagesViewModel.cs
+++ NJInsurancePlatform/Models/MessagesViewModel.cs
@@ -4,5 +4,11 @@
     {
         public IEnumerable<GroupRoom> groupRooms { get; set; }
         public IEnumerable<GroupRoomMessage> groupRoomMessages { get; set; }
+
+        public MessagesViewModel()
+        {
+            this.groupRooms = new List<GroupRoom>();
+            this.groupRoomMessages = new List<GroupRoomMessage>();
+        }
      }
 }
