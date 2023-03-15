fea906738e4dff906649e2f8ed6e2ee473143e14
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:40:05 -0500

    Update IProductRepository.cs

--- NJInsurancePlatform/InterfaceImplementation/IProductRepository.cs
+++ NJInsurancePlatform/InterfaceImplementation/IProductRepository.cs
@@ -6,15 +6,11 @@ namespace NJInsurancePlatform.InterfaceImplementation
     public interface IProductRepository : IDisposable
     {
         Task<IEnumerable<Product>> GetPolicies();
-
         Task<Product> GetPoliciesByID(Guid ProductMUID);
 
         void InsertPolicy(Product product);
-
         void DeletePolicy(Guid ProductMUID);
-
         void UpdatePolicy(Product product);
-
         void Save();
     }
 }
