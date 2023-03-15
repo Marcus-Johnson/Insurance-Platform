08e06fc0fbcc71ccb826d6b59786b6692ec3d855
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:41:08 -0500

    Update iClaimRepository.cs

--- NJInsurancePlatform/InterfaceImplementation/iClaimRepository.cs
+++ NJInsurancePlatform/InterfaceImplementation/iClaimRepository.cs
@@ -6,15 +6,11 @@ namespace NJInsurancePlatform.InterfaceImplementation
     public interface iClaimRepository : IDisposable
     {
         Task<IEnumerable<Claim>> GetClaims();
-
         Task<Claim> GetClaimsByID(Guid ClaimMUID);
 
         void InsertClaim(Claim claim);
-
         void DeleteClaim(Guid GroupMUID);
-
         void UpdateClaim(Claim claim);
-
         void Save();
     }
 }
