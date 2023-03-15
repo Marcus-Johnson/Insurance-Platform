ca787c09e845beaf1bab79fe57bf04cb39902169
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:54:22 -0500

    Update CustomerRepository.cs

--- NJInsurancePlatform/Interfaces/CustomerRepository.cs
+++ NJInsurancePlatform/Interfaces/CustomerRepository.cs
@@ -3,70 +3,46 @@ using Microsoft.EntityFrameworkCore;
 using NJInsurancePlatform.Data;
 using System;
 using NJInsurancePlatform.InterfaceImplementation;
+using System.Threading.Tasks;
 
 namespace NJInsurancePlatform.Interfaces
 {
     public class CustomerRepository : ICustomerRepository, IDisposable
     {
         private readonly InsuranceCorpDbContext _dbContext;
-        private bool disposed = false;
-        
+        private bool disposed;
+
         public CustomerRepository(InsuranceCorpDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        
         public async Task<IEnumerable<Customer>> GetCustomer()
-        {
-            return await _dbContext.Customers.ToListAsync();
-        }
+            => await _dbContext.Customers.ToListAsync();
 
-        
         public async Task<Customer> GetCustomerById(Guid CustomerMUID)
-        {
-            var customer = await _dbContext.Customers.FindAsync(CustomerMUID);
-            return customer;
-        }
-        
-        public async void InsertCustomer(Customer customer)
-        {
-            await _dbContext.Customers.AddAsync(customer);
-        }
-        
-        public async void DeleteCustomer(Guid CustomerMUID)
+            => await _dbContext.Customers.FindAsync(CustomerMUID);
+
+        public async Task InsertCustomer(Customer customer)
+            => await _dbContext.Customers.AddAsync(customer ?? throw new ArgumentNullException(nameof(customer)));
+
+        public async Task DeleteCustomer(Guid CustomerMUID)
         {
             var customer = await _dbContext.Customers.FirstOrDefaultAsync(p => p.CustomerMUID == CustomerMUID);
-            _dbContext.Customers.Remove(customer);
+            _dbContext.Customers.Remove(customer != null ? customer : throw new ArgumentNullException(nameof(customer)));
         }
-        
-        public async void UpdateCustomer(Customer customer)
-        {
-            try
-            {
-                _dbContext.Update(customer);
-            }
-            catch
-            {
-                throw;
-            }
-        }
-        
-        public async void Save()
-        {
-            await _dbContext.SaveChangesAsync();
-        }
+
+        public async Task UpdateCustomer(Customer customer)
+            => _dbContext.Update(customer ?? throw new ArgumentNullException(nameof(customer)));
+
+        public async Task Save()
+            => await _dbContext.SaveChangesAsync();
 
         protected virtual void Dispose(bool disposing)
         {
-            if (!this.disposed)
-            {
-                if (disposing)
-                {
-                    _dbContext.Dispose();
-                }
-            }
-            this.disposed = true;
+            if (disposed) return;
+            if (disposing) _dbContext.Dispose();
+            disposed = true;
         }
 
         public void Dispose()
