2c0623504cc0d915f9cb03378f5296fa8ce14170
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:43:23 -0500

    Update iTransactionRepository.cs

--- NJInsurancePlatform/InterfaceImplementation/iTransactionRepository.cs
+++ NJInsurancePlatform/InterfaceImplementation/iTransactionRepository.cs
@@ -6,15 +6,11 @@ namespace NJInsurancePlatform.InterfaceImplementation
     public interface ITransactionRepository : IDisposable
     {
         Task<List<Transaction>> GetTransactions();
-
         Task<Transaction> GetTransactionsByID(Guid TransactionMUID);
 
         void InsertTransaction(Transaction transaction);
-
         void DeleteTransaction(Guid TransactionMUID);
-
         void UpdateTransaction(Transaction transaction);
-
         void Save();
     }
 }
