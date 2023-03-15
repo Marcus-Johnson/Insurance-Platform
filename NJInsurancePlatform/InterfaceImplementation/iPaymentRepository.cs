9e74f2b58515b411997ee66f54210c9e07854f18
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:42:06 -0500

    Update iPaymentRepository.cs

--- NJInsurancePlatform/InterfaceImplementation/iPaymentRepository.cs
+++ NJInsurancePlatform/InterfaceImplementation/iPaymentRepository.cs
@@ -6,15 +6,11 @@ namespace NJInsurancePlatform.InterfaceImplementation
     public interface IPaymentRepository : IDisposable
     {
         Task<IEnumerable<Payment>> GetPayment();
-
         Task<Payment> GetPaymentsByID(Guid PaymentMUID);
 
         void InsertPayment(Payment payment);
-
         void DeletePayment(Guid PaymentMUID);
-
         void UpdatePayment(Payment payment);
-
         void Save();
     }
 }
