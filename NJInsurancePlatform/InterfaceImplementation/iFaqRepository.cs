2a0927f4ef39ea9c4c4c31b81a5266b04ba4f2ec
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:41:22 -0500

    Update iFaqRepository.cs

--- NJInsurancePlatform/InterfaceImplementation/iFaqRepository.cs
+++ NJInsurancePlatform/InterfaceImplementation/iFaqRepository.cs
@@ -6,15 +6,11 @@ namespace NJInsurancePlatform.InterfaceImplementation
     public interface iFaqRepository : IDisposable
     {
         Task<IEnumerable<Faq>> GetFaqs();
-
         Task<Faq> GetFaqsByID(Guid FaqMUID);
 
         void InsertFaq(Faq faq);
-
         void DeleteFaq(Guid FaqMUID);
-
         void UpdateFaq(Faq faq);
-
         void Save();
     }
 }
