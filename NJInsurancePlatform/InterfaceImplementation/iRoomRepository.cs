542573666fb6a7aa968465dde24648d9cd7967cc
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:43:09 -0500

    Update iRoomRepository.cs

--- NJInsurancePlatform/InterfaceImplementation/iRoomRepository.cs
+++ NJInsurancePlatform/InterfaceImplementation/iRoomRepository.cs
@@ -6,15 +6,11 @@ namespace NJInsurancePlatform.InterfaceImplementation
     public interface iRoomRepository : IDisposable
     {
         Task<List<GroupRoom>> GetGroupRooms();
-
         Task<GroupRoom> GetGroupRoomsByID(Guid GroupMUID);
 
-        void InsertGroupRoom(GroupRoom groupRoom);
-
+        void InsertGroupRoom(GroupRoom groupRoom)
         void DeleteGroupRoom(Guid GroupMUID);
-
         void UpdateGroupRoom(GroupRoom groupRoom);
-
         void Save();
     }
 }
