d0263343bf618b6b90d326eb2d40cb40f5c80559
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:40:54 -0500

    Update iBillRepository.cs

--- NJInsurancePlatform/InterfaceImplementation/iBillRepository.cs
+++ NJInsurancePlatform/InterfaceImplementation/iBillRepository.cs
@@ -6,15 +6,11 @@ namespace NJInsurancePlatform.InterfaceImplementation
     public interface iBillRepository : IDisposable
     {
         Task<List<Bill>> GetBills();
-
         Task<Bill> GetBillsByID(Guid BillMUID);
 
         void InsertBill(Bill bill);
-
         void DeleteBill(Guid bill);
-
         void UpdateBill(Bill bill);
-
         void Save();
     }
 }
