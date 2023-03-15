1d2c9f6d07d16d5fccae82f331e79896aa60f4c8
Marcus J <50967585+Marcus-Johnson@users.noreply.github.com>
2023-03-15 18:40:33 -0500

    Update iBeneficiaryRepository.cs

--- NJInsurancePlatform/InterfaceImplementation/iBeneficiaryRepository.cs
+++ NJInsurancePlatform/InterfaceImplementation/iBeneficiaryRepository.cs
@@ -6,15 +6,11 @@ namespace NJInsurancePlatform.InterfaceImplementation
     public interface iBeneficiaryRepository  : IDisposable
     {
         Task<IEnumerable<Beneficiary>> GetBeneficiaries();
-
         Task<Customer> GetBeneficiariesByID(Guid CustomerMUID);
 
         void InsertBeneficiary(Beneficiary beneficiary);
-
         void DeleteBeneficiary(Guid BeneficiaryMUID);
-
         void UpdateBeneficiary(Beneficiary beneficiary);
-
         void Save();
     }
 }
